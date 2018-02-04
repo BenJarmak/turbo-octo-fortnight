f0b1b37c0af4073a52fb6d2472be99c9370020f1
Ben J <ben.jarmak@gmail.com>
2018-02-04 16:40:45 -0500

    2-3-18 update

--- Homework1.py
+++ Homework1.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def findNearestEuclidean(vector, test_data, k=1, threshold=0, distance_scale=0, individual_scale=0):
+def findNearestEuclidean(vector, test_data, k=1, threshold=0, distance_scale=0.0, individual_scale=0):
     # 90% accurate on training data with k=1 and threshold = 0
     # 80.6% accurate on training data with k=3 and threshold = 0
     # 80.8% accurate on training data with k=3 and threshold = 25
@@ -15,10 +15,12 @@ def findNearestEuclidean(vector, test_data, k=1, threshold=0, distance_scale=0, 
                 line[i] = 0
             if individual_scale != 0:
                 # print((vector[i] - line[i])**2)
-                if (vector[i] - line[i])**2 < individual_scale:
-                    distance += 0.5*(vector[i] - line[i])**2
+                d = (vector[i] - line[i])**2
+                if d < individual_scale:
+                    pass
+                    # distance += 0.5*(vector[i] - line[i])**2
                 else:
-                    distance += 2*(vector[i] - line[i])**2
+                    distance += 2*d
             else:
                 distance += (vector[i] - line[i])**2
         distances.append([np.sqrt(distance), line])
@@ -29,25 +31,80 @@ def findNearestEuclidean(vector, test_data, k=1, threshold=0, distance_scale=0, 
         found_neighbors = []
         for i in range(k):
             if distances[k][0]/distances[0][0] < distance_scale:
-                pass
+                continue
             found_neighbors.append(distances[k][1][-1])
         return max(found_neighbors, key=found_neighbors.count)
 
 
-def findNearestEuclideanModified(vector, test_data, k=1):
+def findNearestManhattan(vector, test_data, k=1, threshold=0, distance_scale=0):
+    # 91.8% accuracy with binarized data at a threshold of 25 and k=1
     distances = []
     for line in test_data:
         distance = 0
-        for i in range(len(vector)):
+        for i in range(len(vector)-1):
+            if vector[i] < threshold:
+                vector[i] = 0
+            if line[i] < threshold:
+                line[i] = 0
             distance += abs(vector[i] - line[i])
         distances.append([distance, line])
-    return min(distances)
+    if k == 1:
+        return min(distances)[1][-1]
+    else:
+        distances.sort(key=lambda x: x[0])
+        found_neighbors = []
+        for i in range(k):
+            if distances[k][0]/distances[0][0] < distance_scale:
+                continue
+            found_neighbors.append(distances[k][1][-1])
+        return max(found_neighbors, key=found_neighbors.count)
 
 
-def findNearestManhattan(vector, test_data, k=1):
-    pass
-    # Find the nearest neighbor using manhattan style distance
+def findNearestMinkowski(vector, test_data, p=1, k=1, threshold=0, distance_scale=0):
+    # 91.8% with p=5 and all other values default
+    distances = []
+    for line in test_data:
+        distance = 0
+        for i in range(len(vector)-1):
+            if vector[i] < threshold:
+                vector[i] = 0
+            if line[i] < threshold:
+                line[i] = 0
+            distance += abs(vector[i] - line[i])**p
+        distances.append([distance**(1/p), line])
+    if k == 1:
+        return min(distances)[1][-1]
+    else:
+        distances.sort(key=lambda x: x[0])
+        found_neighbors = []
+        for i in range(k):
+            if distances[k][0]/distances[0][0] < distance_scale:
+                continue
+            found_neighbors.append(distances[k][1][-1])
+        return max(found_neighbors, key=found_neighbors.count)
 
+def findNearestMahalanobis(vector, test_data, cov_matrix, p=1, k=1, threshold=0, distance_scale=0):
+    # 91.8% with p=5 and all other values default
+    distances = []
+    for line in test_data:
+        line = np.matrix(line)
+        vector = np.matrix(vector)
+        difference = line-vector
+        a = difference * cov_matrix
+        b = difference.T
+        print(a*b)
+        # distance = (np.transpose(vector - line)*cov_matrix*(vector-line)**0.5)
+        # distances.append([distance**(1/p), line])
+    if k == 1:
+        return min(distances)[1][-1]
+    else:
+        distances.sort(key=lambda x: x[0])
+        found_neighbors = []
+        for i in range(k):
+            if distances[k][0]/distances[0][0] < distance_scale:
+                continue
+            found_neighbors.append(distances[k][1][-1])
+        return max(found_neighbors, key=found_neighbors.count)
 
 def stripExtraneous(test_data):
     pass
@@ -100,14 +157,16 @@ for line in test:
         count = 0
 
 success = 0
-# testdata = binarize(testdata)
-# traindata = binarize(traindata)
-for vector in traindata:
-    nearest_neighbor = findNearestEuclidean(vector, testdata, individual_scale=20000)
-    print(vector[-1], nearest_neighbor)
-    if vector[-1] == nearest_neighbor:
-        success += 1
-print(success, success/len(traindata))
+newarray = [np.array(picture[:-1]).reshape((28, 28)) for picture in testdata]
+print (newarray[0])
+# testdata = binarize(testdata, threshold=25)
+# traindata = binarize(traindata, threshold=25)
+# for vector in traindata:
+#     nearest_neighbor = findNearestMahalanobis(vector, testdata, test_cov)
+#     print(vector[-1], nearest_neighbor)
+#     if vector[-1] == nearest_neighbor:
+#         success += 1
+# print(success, success/len(traindata))
 
 # binarized_testdata = binarize(testdata)
 # binarized_traindata = binarize(traindata)
